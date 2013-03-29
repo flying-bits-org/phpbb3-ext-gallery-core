03c3aac9b2233aac2d0ae08811cf923afeb86e7a
Joas Schilling <nickvergessen@gmx.de>
2013-03-29 18:19:43 +0100

    [feature/nestedsets] Try fixing travis once more

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,26 +10,26 @@
 define('IN_PHPBB', true);
 $phpbb_root_path = dirname(__FILE__) . '/vendor/phpBB3/phpBB/';
 $phpEx = 'php';
-require_once $phpbb_root_path . 'includes/startup.php';
+require_once $phpbb_root_path . 'includes/startup.' . $phpEx;
 
 $table_prefix = 'phpbb_';
-require_once $phpbb_root_path . 'includes/constants.php';
+require_once $phpbb_root_path . 'includes/constants.' . $phpEx;
 require_once $phpbb_root_path . 'includes/class_loader.' . $phpEx;
 
-$phpbb_class_loader_mock = new phpbb_class_loader('phpbb_mock_', $phpbb_root_path . '../../../mock/', ".php");
+$phpbb_class_loader_mock = new phpbb_class_loader('phpbb_mock_', $phpbb_root_path . '../../../mock/', '.' . $phpEx);
 $phpbb_class_loader_mock->register();
-$phpbb_class_loader_ext = new phpbb_class_loader('phpbb_ext_', $phpbb_root_path . '../../../../../../', ".php");
+$phpbb_class_loader_ext = new phpbb_class_loader('phpbb_ext_', $phpbb_root_path . '../../../../../../', '.' . $phpEx);
 $phpbb_class_loader_ext->register();
-$phpbb_class_loader = new phpbb_class_loader('phpbb_', $phpbb_root_path . 'includes/', ".php");
+$phpbb_class_loader = new phpbb_class_loader('phpbb_', $phpbb_root_path . 'includes/', '.' . $phpEx);
 $phpbb_class_loader->register();
 
-require_once 'vendor/phpBB3/tests/test_framework/phpbb_test_case_helpers.php';
-require_once 'vendor/phpBB3/tests/test_framework/phpbb_test_case.php';
-require_once 'vendor/phpBB3/tests/test_framework/phpbb_database_test_case.php';
-require_once 'vendor/phpBB3/tests/test_framework/phpbb_database_test_connection_manager.php';
-require_once 'vendor/phpBB3/tests/test_framework/phpbb_functional_test_case.php';
-require_once 'test_framework/gallery_test_case_helpers.php';
-require_once 'test_framework/gallery_test_case.php';
-require_once 'test_framework/gallery_database_test_case.php';
-require_once 'test_framework/gallery_database_test_connection_manager.php';
-require_once 'test_framework/gallery_functional_test_case.php';
+require_once 'vendor/phpBB3/tests/test_framework/phpbb_test_case_helpers.' . $phpEx;
+require_once 'vendor/phpBB3/tests/test_framework/phpbb_test_case.' . $phpEx;
+require_once 'vendor/phpBB3/tests/test_framework/phpbb_database_test_case.' . $phpEx;
+require_once 'vendor/phpBB3/tests/test_framework/phpbb_database_test_connection_manager.' . $phpEx;
+require_once 'vendor/phpBB3/tests/test_framework/phpbb_functional_test_case.' . $phpEx;
+require_once 'test_framework/gallery_test_case_helpers.' . $phpEx;
+require_once 'test_framework/gallery_test_case.' . $phpEx;
+require_once 'test_framework/gallery_database_test_case.' . $phpEx;
+require_once 'test_framework/gallery_database_test_connection_manager.' . $phpEx;
+require_once 'test_framework/gallery_functional_test_case.' . $phpEx;
