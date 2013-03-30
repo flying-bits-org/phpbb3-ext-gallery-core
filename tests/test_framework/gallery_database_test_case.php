992ce56d94297d60dd275377e74169cd8a874469
Joas Schilling <nickvergessen@gmx.de>
2013-03-30 08:39:35 +0100

    [feature/nestedsets] Fix database tests

--- tests/test_framework/gallery_database_test_case.php
+++ tests/test_framework/gallery_database_test_case.php
@@ -6,12 +6,45 @@
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
+require_once dirname(__FILE__) . '/../vendor/phpBB3/phpBB/includes/db/db_tools.php';
 
 abstract class phpbb_ext_gallery_database_test_case extends phpbb_database_test_case
 {
+	protected $db;
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->db = $this->new_dbal();
+
+		global $phpbb_root_path, $phpEx;
+
+		$config = new phpbb_config(array());
+		$db_tools = new phpbb_db_tools($this->db);
+		$table_prefix = 'phpbb_';
+
+		$migrator = new phpbb_db_migrator($config, $this->db, $db_tools, 'phpbb_migrations', $phpbb_root_path, $phpEx, $table_prefix, array());
+
+		$phpbb_extension_manager =  new phpbb_extension_manager(new phpbb_mock_container_builder(), $this->db, $config, $migrator, 'phpbb_ext', dirname(__FILE__) . '/', '.' . $phpEx, null);
+		$phpbb_extension_manager->enable('gallery/core');
+	}
+
 	protected function create_connection_manager($config)
 	{
 		return new phpbb_ext_gallery_database_test_connection_manager($config);
+	}
+
+	public function get_database_config()
+	{
+		$config = phpbb_ext_gallery_test_case_helpers::get_test_config();
+
+		if (!isset($config['dbms']))
+		{
+			$this->markTestSkipped('Missing test_config.php: See first error.');
+		}
+
+		return $config;
 	}
 
 	public function get_test_case_helpers()
--- tests/test_framework/gallery_database_test_connection_manager.php
+++ tests/test_framework/gallery_database_test_connection_manager.php
@@ -9,17 +9,4 @@
 
 class phpbb_ext_gallery_database_test_connection_manager extends phpbb_database_test_connection_manager
 {
-	/**
-	* Load the phpBB database schema into the database
-	*/
-	public function load_schema()
-	{
-		$this->ensure_connected(__METHOD__);
-
-		$directory = dirname(__FILE__) . '/../../docs/schemas/';
-		$this->load_schema_from_file($directory);
-
-		// Also load the phpBB schema's
-		parent::load_schema();
-	}
 }
