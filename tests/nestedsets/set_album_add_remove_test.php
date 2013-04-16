2e0ae07acc460d761cdbea4cda34c160b77cf905
Joas Schilling <nickvergessen@gmx.de>
2013-04-16 03:47:19 +0200

    [feature/nestedsets] Add unit tests for add() and insert()

--- tests/nestedsets/set_album_add_remove_test.php
+++ tests/nestedsets/set_album_add_remove_test.php
@@ -9,9 +9,9 @@
 
 require_once dirname(__FILE__) . '/set_album_base.php';
 
-class phpbb_ext_gallery_tests_nestedsets_set_album_remove_test extends phpbb_ext_gallery_tests_nestedsets_set_album_base
+class phpbb_ext_gallery_tests_nestedsets_set_album_add_remove_test extends phpbb_ext_gallery_tests_nestedsets_set_album_base
 {
-	public function remove_data()
+	public function remove_add_data()
 	{
 		return array(
 			array(1, array(1, 2, 3), array(
@@ -28,6 +28,28 @@ class phpbb_ext_gallery_tests_nestedsets_set_album_remove_test extends phpbb_ext
 				array('album_id' => 11, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 14, 'right_id' => 15, 'album_parents' => ''),
 				array('album_id' => 12, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 17, 'right_id' => 18, 'album_parents' => ''),
 				array('album_id' => 13, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 19, 'right_id' => 20, 'album_parents' => ''),
+				array('album_id' => 14, 'parent_id' => 0, 'user_id' => 2, 'left_id' => 1, 'right_id' => 2, 'album_parents' => ''),
+				array('album_id' => 15, 'parent_id' => 0, 'user_id' => 3, 'left_id' => 1, 'right_id' => 6, 'album_parents' => ''),
+				array('album_id' => 16, 'parent_id' => 15, 'user_id' => 3, 'left_id' => 2, 'right_id' => 3, 'album_parents' => ''),
+				array('album_id' => 17, 'parent_id' => 15, 'user_id' => 3, 'left_id' => 4, 'right_id' => 5, 'album_parents' => ''),
+			), array(
+				1	=> array('parent_id' => 0, 'left_id' => 21, 'right_id' => 22, 'album_parents' => ''),
+				2	=> array('parent_id' => 0, 'left_id' => 23, 'right_id' => 24, 'album_parents' => ''),
+				3	=> array('parent_id' => 0, 'left_id' => 25, 'right_id' => 26, 'album_parents' => ''),
+			), array(
+				array('album_id' => 4, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 1, 'right_id' => 6, 'album_parents' => ''),
+				array('album_id' => 5, 'parent_id' => 4, 'user_id' => 0, 'left_id' => 2, 'right_id' => 5, 'album_parents' => ''),
+				array('album_id' => 6, 'parent_id' => 5, 'user_id' => 0, 'left_id' => 3, 'right_id' => 4, 'album_parents' => ''),
+				array('album_id' => 7, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 7, 'right_id' => 16, 'album_parents' => ''),
+				array('album_id' => 8, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 8, 'right_id' => 9, 'album_parents' => ''),
+				array('album_id' => 9, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 10, 'right_id' => 13, 'album_parents' => ''),
+				array('album_id' => 10, 'parent_id' => 9, 'user_id' => 0, 'left_id' => 11, 'right_id' => 12, 'album_parents' => ''),
+				array('album_id' => 11, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 14, 'right_id' => 15, 'album_parents' => ''),
+				array('album_id' => 12, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 17, 'right_id' => 18, 'album_parents' => ''),
+				array('album_id' => 13, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 19, 'right_id' => 20, 'album_parents' => ''),
+				array('album_id' => 1, 'parent_id' => 0, 'user_id' => 0, 'left_id' =>21, 'right_id' => 22, 'album_parents' => ''),
+				array('album_id' => 2, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 23, 'right_id' => 24, 'album_parents' => ''),
+				array('album_id' => 3, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 25, 'right_id' => 26, 'album_parents' => ''),
 				array('album_id' => 14, 'parent_id' => 0, 'user_id' => 2, 'left_id' => 1, 'right_id' => 2, 'album_parents' => ''),
 				array('album_id' => 15, 'parent_id' => 0, 'user_id' => 3, 'left_id' => 1, 'right_id' => 6, 'album_parents' => ''),
 				array('album_id' => 16, 'parent_id' => 15, 'user_id' => 3, 'left_id' => 2, 'right_id' => 3, 'album_parents' => ''),
@@ -51,23 +73,58 @@ class phpbb_ext_gallery_tests_nestedsets_set_album_remove_test extends phpbb_ext
 				array('album_id' => 15, 'parent_id' => 0, 'user_id' => 3, 'left_id' => 1, 'right_id' => 6, 'album_parents' => ''),
 				array('album_id' => 16, 'parent_id' => 15, 'user_id' => 3, 'left_id' => 2, 'right_id' => 3, 'album_parents' => ''),
 				array('album_id' => 17, 'parent_id' => 15, 'user_id' => 3, 'left_id' => 4, 'right_id' => 5, 'album_parents' => ''),
+			), array(
+				2	=> array('parent_id' => 0, 'left_id' => 25, 'right_id' => 26, 'album_parents' => '')
+			), array(
+				array('album_id' => 1, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 1, 'right_id' => 4, 'album_parents' => ''),
+				array('album_id' => 3, 'parent_id' => 1, 'user_id' => 0, 'left_id' => 2, 'right_id' => 3, 'album_parents' => ''),
+				array('album_id' => 4, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 5, 'right_id' => 10, 'album_parents' => ''),
+				array('album_id' => 5, 'parent_id' => 4, 'user_id' => 0, 'left_id' => 6, 'right_id' => 9, 'album_parents' => ''),
+				array('album_id' => 6, 'parent_id' => 5, 'user_id' => 0, 'left_id' => 7, 'right_id' => 8, 'album_parents' => ''),
+				array('album_id' => 7, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 11, 'right_id' => 20, 'album_parents' => ''),
+				array('album_id' => 8, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 12, 'right_id' => 13, 'album_parents' => ''),
+				array('album_id' => 9, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 14, 'right_id' => 17, 'album_parents' => ''),
+				array('album_id' => 10, 'parent_id' => 9, 'user_id' => 0, 'left_id' => 15, 'right_id' => 16, 'album_parents' => ''),
+				array('album_id' => 11, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 18, 'right_id' => 19, 'album_parents' => ''),
+				array('album_id' => 12, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 21, 'right_id' => 22, 'album_parents' => ''),
+				array('album_id' => 13, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 23, 'right_id' => 24, 'album_parents' => ''),
+				array('album_id' => 2, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 25, 'right_id' => 26, 'album_parents' => ''),
+				array('album_id' => 14, 'parent_id' => 0, 'user_id' => 2, 'left_id' => 1, 'right_id' => 2, 'album_parents' => ''),
+				array('album_id' => 15, 'parent_id' => 0, 'user_id' => 3, 'left_id' => 1, 'right_id' => 6, 'album_parents' => ''),
+				array('album_id' => 16, 'parent_id' => 15, 'user_id' => 3, 'left_id' => 2, 'right_id' => 3, 'album_parents' => ''),
+				array('album_id' => 17, 'parent_id' => 15, 'user_id' => 3, 'left_id' => 4, 'right_id' => 5, 'album_parents' => ''),
 			)),
 		);
 	}
 
 	/**
-	* @dataProvider remove_data
+	* @dataProvider remove_add_data
 	*/
-	public function test_remove($album_id, $expected_removed, $expected)
+	public function test_remove_add($album_id, $expected_removed, $expected_remove_table, $expected_added, $expected_add_table)
 	{
 		$album = new phpbb_ext_gallery_core_nestedsets_item_album($this->album_data[$album_id]);
 
-		$this->assertEquals($expected_removed, $this->set->remove($album));
+		$removed_items = $this->set->remove($album);
+
+		$this->assertEquals($expected_removed, $removed_items);
 
 		$result = $this->db->sql_query("SELECT album_id, parent_id, user_id, left_id, right_id, album_parents
 			FROM phpbb_gallery_albums
 			ORDER BY user_id, left_id, album_id ASC");
-		$this->assertEquals($expected, $this->db->sql_fetchrowset($result));
+		$this->assertEquals($expected_remove_table, $this->db->sql_fetchrowset($result));
+
+		$added_items = array();
+		foreach ($removed_items as $item_id)
+		{
+			$album = new phpbb_ext_gallery_core_nestedsets_item_album($this->album_data[$item_id]);
+			$added_items[$item_id] = $this->set->add($album);
+		}
+		$this->assertEquals($expected_added, $added_items);
+
+		$result = $this->db->sql_query("SELECT album_id, parent_id, user_id, left_id, right_id, album_parents
+			FROM phpbb_gallery_albums
+			ORDER BY user_id, left_id, album_id ASC");
+		$this->assertEquals($expected_add_table, $this->db->sql_fetchrowset($result));
 	}
 
 	public function delete_data()
@@ -122,6 +179,59 @@ class phpbb_ext_gallery_tests_nestedsets_set_album_remove_test extends phpbb_ext
 		$result = $this->db->sql_query("SELECT album_id, parent_id, user_id, left_id, right_id, album_parents
 			FROM phpbb_gallery_albums
 			ORDER BY user_id, left_id, album_id ASC");
+		$this->assertEquals($expected, $this->db->sql_fetchrowset($result));
+	}
+	public function insert_data()
+	{
+		return array(
+			array(array(
+				'album_desc' => '',
+				'album_id' => 18,
+				'parent_id' => 0,
+				'user_id' => 0,
+				'left_id' => 27,
+				'right_id' => 28,
+				'album_parents' => '',
+			), array(
+				array('album_id' => 1, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 1, 'right_id' => 6, 'album_parents' => ''),
+				array('album_id' => 2, 'parent_id' => 1, 'user_id' => 0, 'left_id' => 2, 'right_id' => 3, 'album_parents' => ''),
+				array('album_id' => 3, 'parent_id' => 1, 'user_id' => 0, 'left_id' => 4, 'right_id' => 5, 'album_parents' => ''),
+
+				array('album_id' => 4, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 7, 'right_id' => 12, 'album_parents' => ''),
+				array('album_id' => 5, 'parent_id' => 4, 'user_id' => 0, 'left_id' => 8, 'right_id' => 11, 'album_parents' => ''),
+				array('album_id' => 6, 'parent_id' => 5, 'user_id' => 0, 'left_id' => 9, 'right_id' => 10, 'album_parents' => ''),
+
+				array('album_id' => 7, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 13, 'right_id' => 22, 'album_parents' => ''),
+				array('album_id' => 8, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 14, 'right_id' => 15, 'album_parents' => ''),
+				array('album_id' => 9, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 16, 'right_id' => 19, 'album_parents' => ''),
+				array('album_id' => 10, 'parent_id' => 9, 'user_id' => 0, 'left_id' => 17, 'right_id' => 18, 'album_parents' => ''),
+				array('album_id' => 11, 'parent_id' => 7, 'user_id' => 0, 'left_id' => 20, 'right_id' => 21, 'album_parents' => ''),
+
+				array('album_id' => 12, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 23, 'right_id' => 24, 'album_parents' => ''),
+				array('album_id' => 13, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 25, 'right_id' => 26, 'album_parents' => ''),
+				array('album_id' => 18, 'parent_id' => 0, 'user_id' => 0, 'left_id' => 27, 'right_id' => 28, 'album_parents' => ''),
+
+				array('album_id' => 14, 'parent_id' => 0, 'user_id' => 2, 'left_id' => 1, 'right_id' => 2, 'album_parents' => ''),
+
+				array('album_id' => 15, 'parent_id' => 0, 'user_id' => 3, 'left_id' => 1, 'right_id' => 6, 'album_parents' => ''),
+				array('album_id' => 16, 'parent_id' => 15, 'user_id' => 3, 'left_id' => 2, 'right_id' => 3, 'album_parents' => ''),
+				array('album_id' => 17, 'parent_id' => 15, 'user_id' => 3, 'left_id' => 4, 'right_id' => 5, 'album_parents' => ''),
+			)),
+		);
+	}
+
+	/**
+	* @dataProvider insert_data
+	*/
+	public function test_insert($expected_data, $expected)
+	{
+		$this->assertEquals($expected_data, $this->set->insert(array(
+			'album_desc' => '',
+		)));
+
+		$result = $this->db->sql_query('SELECT album_id, parent_id, user_id, left_id, right_id, album_parents
+			FROM phpbb_gallery_albums
+			ORDER BY user_id, left_id, album_id ASC');
 		$this->assertEquals($expected, $this->db->sql_fetchrowset($result));
 	}
 }
