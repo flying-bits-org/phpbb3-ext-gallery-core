a076eef07f4b3963a0e55a62a3d7773e3172197f
Joas Schilling <nickvergessen@gmx.de>
2013-05-03 11:28:36 +0200

    [feature/nestedset-album] Use new nestedset tree class from the core

--- album/nestedset.php
+++ album/nestedset.php
@@ -15,69 +15,35 @@ if (!defined('IN_PHPBB'))
 	exit;
 }
 
-class phpbb_ext_gallery_core_album_nestedset extends phpbb_ext_gallery_core_nestedsets_abstract
+class phpbb_ext_gallery_core_album_nestedset extends phpbb_tree_nestedset
 {
-	/** @var phpbb_db_driver */
-	protected $db;
-
-	/** @var phpbb_lock_db */
-	protected $lock;
-
-	/** @var String */
-	protected $table_name;
-
-	/** @var String */
-	protected $item_class = 'phpbb_ext_gallery_core_album_nestedsetitem';
-
-	/**
-	* Column names in the table
-	* @var array
-	*/
-	protected $column_item_id = 'album_id';
-	protected $column_item_parents = 'album_parents';
-	protected $table_columns = array(
-		'item_id'	=> 'album_id',
-		'left_id'	=> 'left_id',
-		'right_id'	=> 'right_id',
-		'parent_id'	=> 'parent_id',
-		'item_parents'	=> 'album_parents',
-		'user_id',
-	);
-
-	/**
-	* Additional SQL restrictions
-	* Allows to have multiple nestedsets in one table
-	* Columns must be prefixed with %1$s
-	* @var String
-	*/
-	protected $sql_where = '';
-
-	/**
-	* Album set owner user_id
-	* @var int
-	*/
-	protected $user_id;
-
-	/**
-	* List of item properties to be cached in $item_parents
-	* @var array
-	*/
-	protected $item_basic_data = array('album_id', 'album_name', 'album_type');
-
 	/**
 	* Construct
 	*
 	* @param phpbb_db_driver	$db		Database connection
-	* @param phpbb_lock_db		$lock	Lock class used to lock the table when moving albums around
+	* @param phpbb_lock_db		$lock	Lock class used to lock the table when moving forums around
 	* @param string				$table_name		Table name
 	*/
 	public function __construct(phpbb_db_driver $db, phpbb_lock_db $lock, $table_name)
 	{
-		$this->db = $db;
-		$this->lock = $lock;
-		$this->table_name = $table_name;
 		$this->user_id = 0;
-		$this->sql_where = '%1$s' . 'user_id = 0';
+
+		parent::__construct(
+			$db,
+			$lock,
+			$table_name,
+			'GALLERY_ALBUM_',
+			'%1$s' . 'user_id = ' . $this->user_id,
+			array(
+				'album_id',
+				'album_name',
+				'album_type',
+			),
+			array(
+				'item_id'		=> 'album_id',
+				'item_parents'	=> 'album_parents',
+			)
+		);
 	}
 
 	/**
@@ -101,55 +67,5 @@ class phpbb_ext_gallery_core_album_nestedset extends phpbb_ext_gallery_core_nest
 		));
 
 		return parent::insert($item_data);
-	}
-
-	/**
-	* @inheritdoc
-	*/
-	public function move_children(phpbb_ext_gallery_core_nestedsets_item_interface $current_parent, phpbb_ext_gallery_core_nestedsets_item_interface $new_parent)
-	{
-		while (!$this->lock->acquire())
-		{
-			// Retry after 0.2 seconds
-			usleep(200 * 1000);
-		}
-
-		try
-		{
-			$return = parent::move_children($current_parent, $new_parent);
-		}
-		catch (phpbb_ext_gallery_core_nestedsets_exception $e)
-		{
-			$this->lock->release();
-			throw new phpbb_ext_gallery_core_exception('GALLERY_ALBUM_' . $e->getMessage());
-		}
-		$this->lock->release();
-
-		return $return;
-	}
-
-	/**
-	* @inheritdoc
-	*/
-	public function set_parent(phpbb_ext_gallery_core_nestedsets_item_interface $item, phpbb_ext_gallery_core_nestedsets_item_interface $new_parent)
-	{
-		while (!$this->lock->acquire())
-		{
-			// Retry after 0.2 seconds
-			usleep(200 * 1000);
-		}
-
-		try
-		{
-			$return = parent::set_parent($item, $new_parent);
-		}
-		catch (phpbb_ext_gallery_core_nestedsets_exception $e)
-		{
-			$this->lock->release();
-			throw new phpbb_ext_gallery_core_exception('GALLERY_ALBUM_' . $e->getMessage());
-		}
-		$this->lock->release();
-
-		return $return;
 	}
 }
